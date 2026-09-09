4e9657b437ecf69c23aff893f41b563cee44ba01
Lakshya Veyyakula <veyyakulakishore@gmail.com>
2026-09-09 00:15:06 -0600

    c

--- app/src/main/java/com/example/listcity/MainActivity.kt
+++ app/src/main/java/com/example/listcity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,11 +25,13 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.listcity.ui.theme.ListCityTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +44,7 @@ class MainActivity : ComponentActivity() {
                     CityListScreen(
                         cities = cityRepository.cities,
                         onAddCity = { cityRepository.addCity(it) },
+                        onDeleteCity = { cityRepository.deleteCity(it) },
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -47,58 +53,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-@Composable
-fun CityListScreen(
-    cities: List<String>,
-    onAddCity: (String) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    var newCityName by remember {mutableStateOf(value ="")}
-
-    Column(modifier = modifier.fillMaxSize()) {
-        Row(modifier = Modifier.padding(all = 16.dp)) {
-            OutlinedTextField(
-                value = newCityName,
-                onValueChange = {newCityName = it },
-                label = { Text("City name") },
-                modifier = Modifier.weight(1f)
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Button(
-                OnClick = {
-                    if (newCityName.isNotBlank()) {
-                        onAddCity(newCityName)
-                        newCityName = ""
-                    }
-                }
-            ) {
-                Text("Add City")
-            }
-        }
-    }
-    LazyColumn(modifier = modifier.fillMaxSize()) {
-        items(cities) { city ->
-            CityRow(city = city)
-        }
-    }
-}
-@Composable
-fun CityRow(city: String) {
-    Text(
-        text = city,
-        fontSize = 28.sp,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 18.dp, vertical = 14.dp)
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ListCityTheme {
-        Greeting("Android")
+   //     Greeting("Android")
     }
 }
 
@@ -113,5 +72,73 @@ class CityRepository {
 
     fun addCity(city: String) {
         _cities.add(city)
+    }
+
+    fun deleteCity(city: String) {
+        _cities.remove(city)
+    }
+}
+
+@Composable
+fun CityListScreen(
+    cities: List<String>,
+    onAddCity: (String) -> Unit,
+    onDeleteCity: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var newCityName by remember { mutableStateOf(value = "") }
+
+    Column(modifier = modifier.fillMaxSize()) {
+        Row(modifier = Modifier.padding(all = 16.dp)) {
+            OutlinedTextField(
+                value = newCityName,
+                onValueChange = { newCityName = it },
+                label = { Text("City name") },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                onClick = {
+                    if (newCityName.isNotBlank()) {
+                        onAddCity(newCityName)
+                        newCityName = ""
+                    }
+                }
+            ) {
+                Text("Add City")
+            }
+        }
+
+        LazyColumn {
+            items(cities) { city ->
+                CityRow(
+                    city = city,
+                    onDeleteClick = { onDeleteCity(city) })
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CityRow(
+    city: String,
+    onDeleteClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 18.dp, vertical = 14.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = city,
+            fontSize = 28.sp,
+            modifier = Modifier.weight(1f)
+        )
+        OutlinedButton(
+            onClick = onDeleteClick,
+        ) {
+            Text("Delete")
+        }
     }
 }
